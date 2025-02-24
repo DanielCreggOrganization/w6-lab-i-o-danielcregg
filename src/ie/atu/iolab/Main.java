24b75f941b0d57f9d3dd276fdac897a1cc66c8b9
Daniel Cregg <danielcregg@users.noreply.github.com>
2025-02-24 13:48:00 +0000

    Section 2: Implement file path checking for output.txt

--- src/ie/atu/iolab/Main.java
+++ src/ie/atu/iolab/Main.java
@@ -1,7 +1,19 @@
 package ie.atu.iolab;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Path projectRoot = Paths.get(System.getProperty("user.dir"));
+        System.out.println("Project root directory: " + projectRoot.toAbsolutePath());
+        System.out.println("The input.txt file is located at: " + projectRoot.resolve("resources").resolve("input.txt").toAbsolutePath());
+        Path outputFilePath = projectRoot.resolve("resources").resolve("output.txt");
+        if (Files.exists(outputFilePath)) {
+            System.out.println("output.txt found at: " + outputFilePath.toAbsolutePath());
+        } else {
+            System.out.println("output.txt not found at: " + outputFilePath.toAbsolutePath());
+        }
     }
 }
